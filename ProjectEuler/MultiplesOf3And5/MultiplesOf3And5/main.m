1858b82a79291c7daa3a57f221a6802f158f6fd0
wh1pch81n <wh1pch81n@gmail.com>
2013-07-20 23:03:46 -0700

    asd

---
+++ ProjectEuler/MultiplesOf3And5/MultiplesOf3And5/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MultiplesOf3And5
+//
+//  Created by Derrick Ho on 7/20/13.
+//  Copyright (c) 2013 Derrick Ho. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
