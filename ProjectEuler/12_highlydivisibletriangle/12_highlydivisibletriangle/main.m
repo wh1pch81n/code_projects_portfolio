2a09940470c85db24f51e4ffa0edea28919fad12
wh1pch81n <wh1pch81n@gmail.com>
2013-08-04 14:54:14 -0700

    second attempt using primes and some co primes.
    Still doesn't work.  too slow.
    a brute force approch of someone else discovered a solution in less than
    a second...and it was still considered slow...

--- ProjectEuler/12_highlydivisibletriangle/12_highlydivisibletriangle/main.m
+++ ProjectEuler/12_highlydivisibletriangle/12_highlydivisibletriangle/main.m
@@ -14,6 +14,7 @@
 - (id)init;
 @property (strong, nonatomic) NSMutableArray *smallPrimeList;
 @property (strong, nonatomic) NSMutableDictionary *primeCount;
+@property (strong, nonatomic) NSMutableSet *divisors;
 @end
 @implementation Factorize
 - (id)init{
@@ -25,6 +26,7 @@
 	return self;
 }
 - (NSArray *)factorsOfNum:(NSNumber *)num {
+	[self setPrimeCount:[NSMutableDictionary new]];
 	while ([num integerValue] > 1) {
 		for (NSNumber *n in [self smallPrimeList]) {
 			if ([num integerValue] % [n integerValue] == 0) {
@@ -38,12 +40,19 @@
 	}]];
 }
 - (NSNumber *)numOfDivisors:(NSNumber *)num {
+	if (num.integerValue == 0) return nil;
 	NSArray *factors = [self factorsOfNum:num];
-	NSInteger total = 0;
+	NSInteger total = (num.integerValue == 1)? 1: 2;
+	NSMutableArray *coPrime = [NSMutableArray new];
 	for (NSNumber *n in factors) {
-		total += [[[self primeCount] objectForKey:n] integerValue];
+		//total += [[[self primeCount] objectForKey:n] integerValue];
+		[coPrime addObject:@([num integerValue]/[n integerValue])];
 	}
-	return @(total);
+	self.divisors = [NSMutableSet new];
+	[self.divisors addObjectsFromArray:factors];
+	[self.divisors addObjectsFromArray:coPrime];
+	[self.divisors addObjectsFromArray:@[@(1), num]];
+	return @(self.divisors.count);
 }
 /**makes an array with size count filled with primes*/
 - (void)makeSmallPrimeListWithCount:(NSInteger)count {
@@ -58,18 +67,38 @@ int main(int argc, const char * argv[])
 {
 
 	@autoreleasepool {
-		int total = 0;
-		for (int i = 1; i < 10; ++i) {
-			total += i;
-			printf("%d\n", total);
-		}
+		{
 		Factorize *f = [Factorize new];
-		[f makeSmallPrimeListWithCount:500];
+		int largest = 0;
+		[f makeSmallPrimeListWithCount:10000];
+		long long int total = 0;
+		for (int i = 1; i < 100000; ++i) {
+			total = i*(i + 1)/2;
+			
+			NSNumber *n = [f numOfDivisors:@(total)];
+			if (largest < [n integerValue]) {
+				largest = [n integerValue];
+				NSLog(@"[%d] %lld: %@\n%@", i, total, n,  [f divisors]);
+			}
+			if ([n integerValue] >= 500){
+				break;
+			}
+		}
+		printf("done\n");
+		}
+//		int total = 0;
+//		for (int i = 1; i < 10; ++i) {
+//			total += i;
+//			NSLog(@"%d: %@\n%@", total, [f numOfDivisors:@(total)],  [f divisors]);
+//		}
+	
 		
-		NSLog(@"%@", [f smallPrimeList]);
-		NSLog(@"%@", [f factorsOfNum:@(21)]);
-		NSLog(@"%@", [f primeCount]);
-		NSLog(@"%@", [f numOfDivisors:@(21)]);
+//		NSLog(@"%@", [f smallPrimeList]);
+//		NSLog(@"%@", [f factorsOfNum:@(21)]);
+//		NSLog(@"%@", [f primeCount]);
+//		NSLog(@"%@", [f numOfDivisors:@(21)]);
+//		total = 28;
+//		NSLog(@"%d: %@\n%@", total, [f numOfDivisors:@(total)],  [f divisors]);
 	}
     return 0;
 }
