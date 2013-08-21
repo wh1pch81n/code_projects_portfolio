c83646e0d11afd40d5be5ee1b5da194c6294d9ce
wh1pch81n <wh1pch81n@gmail.com>
2013-08-21 16:43:50 -0700

    adding merge sort again

---
+++ DataStructuresAndAlgorithms/MergeSortWithBlocks_8-21-2013/MergeSortWithBlocks_8-21-2013/main.m
@@ -0,0 +1,75 @@
+//
+//  main.m
+//  MergeSortWithBlocks_8-21-2013
+//
+//  Created by Derrick Ho on 8/21/13.
+//  Copyright (c) 2013 Derrick Ho. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (MergeSort)
+- (NSArray *)mergeSortWithCompare:(int(^)(int a, int b))compare;
+@end
+@implementation NSArray (MergeSort)
+- (NSArray *)mergeSortWithCompare:(int(^)(int a, int b))compare {
+	if (self.count >1) {
+		return [self mergeArray1:[[self subarrayWithRange:NSMakeRange(0, self.count >> 1)]
+								  mergeSortWithCompare:compare]
+					  withArray2:[[self subarrayWithRange:NSMakeRange(self.count >> 1, self.count - (self.count >> 1))]
+								  mergeSortWithCompare:compare]
+				  withComparator:compare];
+	}
+	return self;
+}
+- (NSArray *)mergeArray1:(NSArray *)arr1 withArray2:(NSArray *)arr2 withComparator:(int(^)(int a, int b))compare{
+	int index_1 , index_2, max_count;
+	index_1 = index_2 = 0;
+	max_count = (int)arr1.count + (int)arr2.count;
+	NSArray *fusion = [NSArray new];
+	for (int i = 0; i < max_count; ++i){
+		if (index_1 >= [arr1 count]) {
+			fusion = [fusion arrayByAddingObject:arr2[index_2++]];
+		} else if (index_2 >= [arr2 count]) {
+			fusion = [fusion arrayByAddingObject:arr1[index_1++]];
+		} else if ( compare( [arr1[index_1] intValue], [arr2[index_2] intValue]) > 0) {
+			fusion = [fusion arrayByAddingObject:arr1[index_1++]];
+		} else if ( compare( [arr1[index_1] intValue], [arr2[index_2] intValue]) <= 0) {
+			fusion = [fusion arrayByAddingObject:arr2[index_2++]];
+		} else {
+			@try {
+				NSException *e = [NSException exceptionWithName:@"Unknown"
+														 reason:@"unHandled" userInfo:nil];
+				@throw e;
+			} @catch (NSException *theException) {
+				if ([[theException name] isEqualToString:@"Unknown"]) {
+					NSLog(@"caught something weird");
+					exit(-1);
+				}
+			}
+		}
+	}
+	return fusion;
+}
+@end
+
+int main(int argc, const char * argv[])
+{
+	@autoreleasepool {
+		__block int(^bigToSmall)(int a, int b) = ^int(int a, int b) {
+			return a - b;
+		};
+		__block int(^smallToBig)(int a, int b) = ^int(int a, int b) {
+			return b - a;
+		};
+	    NSMutableArray *marr = [NSMutableArray new];
+		for (int i = 0; i < 1000; ++i) {
+			[marr addObject:@(arc4random() % 100)];
+		}
+	    NSLog(@"%@", marr);
+		NSLog(@"%@", [marr mergeSortWithCompare:bigToSmall]);
+		NSLog(@"%@", [marr mergeSortWithCompare:smallToBig]);
+	}
+    return 0;
+}
+
